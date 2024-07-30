17784786a5b7aa4faf3296a87a16a94b16cc1794
ZachCraw <zachariahcraw@gmail.com>
2024-07-30 13:30:40 +1000

    triangle

--- src/resolvingconflicts.java
+++ src/resolvingconflicts.java
@@ -1,4 +1,14 @@
 public class resolvingconflicts {
     public static void main(String[] args){
+        System.out.println("Bob likes triangles!");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < (4 - i) * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 4 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
