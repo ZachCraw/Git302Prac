94051977fbe34c83456c4dbee08f97d6dc82ecc9
ZachCraw <zachariahcraw@gmail.com>
2024-07-30 13:21:44 +1000

    Added project message

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
         System.out.println("Welcome to CAB302!");
+        System.out.println("This is our first project!");
     }
 }
