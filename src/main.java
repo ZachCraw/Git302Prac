1f8a27a39d0b124b619278e58cdf69cd9ed8c034
ZachCraw <zachariahcraw@gmail.com>
2024-07-30 13:40:21 +1000

    update

--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
         // Test the leftPad method
         System.out.println(leftPad("Hello, world!", 30, ' '));
